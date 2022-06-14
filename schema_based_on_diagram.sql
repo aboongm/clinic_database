6386df08c09c6bfb79251d08ac1d8bb03fbfd75e
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-14 10:42:07 +0200

    Add NON-CLUSTERED Index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -62,3 +62,5 @@ CREATE INDEX patient_id_asc ON medical_histories (patient_id ASC);
 CREATE INDEX medical_history_id_asc ON invoices (medical_histories_id ASC);
 CREATE INDEX invoice_id_asc ON invoice_items (invoice_id ASC);
 CREATE INDEX treatment_id_asc ON invoice_items (treatment_id ASC);
+CREATE INDEX invoice_id_asc ON invoice_items (invoice_id ASC);
+CREATE INDEX treatment_id_asc ON invoice_items (treatment_id ASC);

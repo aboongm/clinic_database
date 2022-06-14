e0042428ed25ec7559e74a4b9f4948a233e72c42
aboongm <aboongm@yahoo.com>
2022-06-14 13:41:47 +0530

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,12 @@ CREATE TABLE patients (
     name VARCHAR(100),
     date_of_birth DATE
 );
+
+/* Create medical histories table */
+CREATE TABLE medical_histories (
+	id	SERIAL PRIMARY KEY,
+	admitted_at	TIMESTAMP,
+	patient_id INT NOT NULL,
+	status VARCHAR,
+	FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
